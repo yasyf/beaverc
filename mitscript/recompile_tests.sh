5f6b7b472723a84fac708866ceddc0e325cf7639
Jack Serrino <Jserrino@gmail.com>
2017-04-07 19:01:18 -0400

    Add recompile script

---
+++ mitscript/recompile_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DEFAULT="\033[39m"
+GREEN="\033[32m"
+
+message() {
+  echo -e "$GREEN $1 $DEFAULT"
+}
+
+compile() {
+  message "Recompiling $1"
+  ./bin/bccompiler < "$1" > "$1bc"
+}
+
+make bccompiler
+
+for f in tests/bytecodetest*.mit
+do
+  compile "$f"
+done
