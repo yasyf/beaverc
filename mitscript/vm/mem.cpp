55190ece52671a7edf348aad8fe898aae62ea37e
Jack Serrino <Jserrino@gmail.com>
2017-04-19 11:36:30 -0400

    Make slightly more portable

--- mitscript/vm/mem.cpp
+++ mitscript/vm/mem.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 size_t rss() {
+  #ifdef MAC_OS
+  return 1024; // Randomly selected for now to make things compile
+  #else
   int ignored, resident;
   ifstream buffer("/proc/self/statm");
   buffer >> ignored >> resident;
   buffer.close();
   return resident * (sysconf(_SC_PAGE_SIZE) / 1024);
+  #endif
 }
