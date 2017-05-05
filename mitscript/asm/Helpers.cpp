f9d2a0f1caf68d11c30c5c8c76db6eac75fddad8
Jack Serrino <jserrino@gmail.com>
2017-05-05 10:45:49 -0400

    Add some debug

--- mitscript/asm/Helpers.cpp
+++ mitscript/asm/Helpers.cpp
@@ -2,18 +2,31 @@
 #include "../vm/globals.h"
 #include "../vm/operations.h"
 
+using namespace std;
 using namespace VM;
 
 namespace ASM {
   void helper_garbage_collect() {
+    #if DEBUG
+      cout << "helper_garbage_collect" << endl;
+    #endif
     interpreter->potentially_garbage_collect();
   };
 
   uint64_t helper_alloc_record() {
+    #if DEBUG
+      cout << "helper_alloc_record" << endl;
+    #endif
     return Value::makePointer(interpreter->heap.allocate<RecordValue>()).value;
   }
 
   uint64_t helper_call_function(uint64_t closure_p, Value* args, int argc) {
+    #if DEBUG
+      cout << "helper_call_function" << endl;
+      cout << "Closure: " << (void*) closure_p << endl;
+      cout << "Args: " << args << endl;
+      cout << "Index: " << argc << endl;
+    #endif
     AbstractFunctionValue* closure = Value(closure_p).getPointer<AbstractFunctionValue>();
     std::vector<Value> values;
     for (size_t i = 0; i < argc; ++i) {
@@ -25,74 +38,124 @@ namespace ASM {
   uint64_t helper_read_global(uint64_t closure_p, int index) {
     ClosureFunctionValue* closure = Value(closure_p).getPointer<ClosureFunctionValue>();
     std::string name = closure->value->names_[index];
+    #if DEBUG
+      cout << "helper_read_global" << endl;
+      cout << "Closure: " << (void*) closure_p << endl;
+      cout << "name: " << name << endl;
+    #endif
     return interpreter->global_variables[name].value;
   }
 
   void helper_write_global(uint64_t closure_p, int index, uint64_t value) {
     ClosureFunctionValue* closure = Value(closure_p).getPointer<ClosureFunctionValue>();
     std::string name = closure->value->names_[index];
+    #if DEBUG
+      cout << "helper_read_global" << endl;
+      cout << "Closure: " << (void*) closure_p << endl;
+      cout << "name: " << name << endl;
+    #endif
     interpreter->global_variables[name].value = value;
   }
 
   uint64_t helper_read_reference(uint64_t reference_p) {
+    #if DEBUG
+      cout << "helper_read_reference" << endl;
+    #endif
     ReferenceValue* reference = (ReferenceValue*) reference_p;
     return reference->value.value;
   }
 
   void helper_write_reference(uint64_t reference_p, uint64_t value) {
+    #if DEBUG
+      cout << "helper_write_reference" << endl;
+    #endif
     ((ReferenceValue*) reference_p)->value.value = value;
   }
 
   uint64_t helper_read_function(uint64_t closure_p, int index) {
+    #if DEBUG
+      cout << "helper_read_function" << endl;
+    #endif
     ClosureFunctionValue* closure = Value(closure_p).getPointer<ClosureFunctionValue>();
     return allocateFunction(closure, index).value;
   }
 
   uint64_t helper_field_load(uint64_t closure_p, uint64_t record_p, int index) {
+    #if DEBUG
+      cout << "helper_field_load" << endl;
+    #endif
     ClosureFunctionValue* closure = Value(closure_p).getPointer<ClosureFunctionValue>();
     RecordValue* record = Value(record_p).getPointer<RecordValue>();
     return record->get(closure->value->names_[index]).value;
   }
 
   void helper_field_store(uint64_t closure_p, uint64_t record_p, int index, uint64_t value) {
+    #if DEBUG
+      cout << "helper_field_store" << endl;
+    #endif
     ClosureFunctionValue* closure = Value(closure_p).getPointer<ClosureFunctionValue>();
     RecordValue* record = Value(record_p).getPointer<RecordValue>();
     record->insert(closure->value->names_[index], Value(value));
   }
 
   uint64_t helper_index_load(uint64_t record_p, uint64_t index) {
+    #if DEBUG
+      cout << "helper_index_load" << endl;
+    #endif
     RecordValue* record = Value(record_p).getPointer<RecordValue>();
     return record->get(Value(index).toString()).value;
   }
 
   void helper_index_store(uint64_t record_p, uint64_t index, uint64_t value) {
+    #if DEBUG
+      cout << "helper_index_store" << endl;
+    #endif
     RecordValue* record = Value(record_p).getPointer<RecordValue>();
     record->insert(Value(index).toString(), Value(value));
   }
 
   void helper_assert_int(uint64_t value) {
+    #if DEBUG
+      cout << "helper_assert_int" << endl;
+    #endif
     Value(value).getInteger();
   }
 
   void helper_assert_not_zero(uint64_t value) {
+    #if DEBUG
+      cout << "helper_assert_not_zero" << endl;
+    #endif
     if (value == 0) {
       throw IllegalArithmeticException("divide by zero");
     }
   }
 
   void helper_assert_bool(uint64_t value) {
+    #if DEBUG
+      cout << "helper_assert_bool" << endl;
+    #endif
     Value(value).getBoolean();
   }
 
   uint64_t helper_add(uint64_t left, uint64_t right) {
+    #if DEBUG
+      cout << "helper_add" << endl;
+    #endif
     return add(Value(left), Value(right)).value;
   }
 
   uint64_t helper_equals(uint64_t left, uint64_t right) {
+    #if DEBUG
+      cout << "helper_equals" << endl;
+    #endif
     return equals(Value(left), Value(right)).value;
   }
 
   void helper_setup_function(Value* arguments, ReferenceValue** refs, Value* base_pointer, uint64_t closure_p) {
+    #if DEBUG
+      cout << "helper_setup_function" << endl;
+    #endif
+
     ClosureFunctionValue* closure = Value(closure_p).getPointer<ClosureFunctionValue>();
 
     #warning Make sure to add locals and temps to the set of roots
