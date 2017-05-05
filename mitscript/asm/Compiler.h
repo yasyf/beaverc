6adb933a270bf7cd3c7905178a99af8ebd9908b2
Yasyf Mohamedali <yasyfm@gmail.com>
2017-05-05 05:23:38 -0400

    IR::Operation::CondJump.

--- mitscript/asm/Compiler.h
+++ mitscript/asm/Compiler.h
@@ -32,30 +32,30 @@ namespace ASM {
       return M64{rbp, Imm32{(uint32_t)(-(i+RESERVED_STACK_SPACE+num_locals) * STACK_VALUE_SIZE)}};
     }
 
-    void read_local(size_t i, const R64& reg) {
-      assm.mov(reg, local(i));
+    void read_local(Var v, const R64& reg) {
+      assm.mov(reg, local(v.num));
     }
 
-    void read_temp(size_t i, const R64& reg) {
-      assm.mov(reg, temp(i));
+    void read_temp(Temp t, const R64& reg) {
+      assm.mov(reg, temp(t.num));
     }
 
-    void write_local(size_t i, const R64& reg) {
-      assm.mov(local(i), reg);
+    void write_local(Var v, const R64& reg) {
+      assm.mov(local(v.num), reg);
     }
 
-    void write_temp(size_t i, const M64& mem) {
+    void write_temp(Temp t, const M64& mem) {
       assm.mov(r10, mem);
-      assm.mov(temp(i), r10);
+      assm.mov(temp(t.num), r10);
     }
 
-    void write_temp(size_t i, const R64& reg) {
-      assm.mov(temp(i), reg);
+    void write_temp(Temp t, const R64& reg) {
+      assm.mov(temp(t.num), reg);
     }
 
-    void write_temp(size_t i, uint64_t cons) {
+    void write_temp(Temp t, uint64_t cons) {
       assm.mov(r10, Imm64{cons});
-      assm.mov(temp(i), r10);
+      assm.mov(temp(t.num), r10);
     }
 
     void call_helper(void* fn, const R64& arg1, const R64& arg2) {
@@ -79,86 +79,97 @@ namespace ASM {
         switch (instruction->op()) {
           case IR::Operation::Assign: {
             if (auto assign = dynamic_cast<Assign<Var>*>(instruction)) {
-              read_local(assign->src.num, r10);
-              write_temp(assign->dest.num, r10);
+              read_local(assign->src, r10);
+              write_temp(assign->dest, r10);
             } else if (auto assign = dynamic_cast<Assign<Const>*>(instruction)) {
-              write_temp(assign->dest.num, assign->src.val);
+              write_temp(assign->dest, assign->src.val);
             } else if (auto assign = dynamic_cast<Assign<RetVal>*>(instruction)) {
-              write_temp(assign->dest.num, rax);
+              write_temp(assign->dest, rax);
             } else if (auto assign = dynamic_cast<Assign<Temp>*>(instruction)) {
-              read_temp(assign->src.num, r10);
-              write_temp(assign->dest.num, r10);
+              read_temp(assign->src, r10);
+              write_temp(assign->dest, r10);
             } else if (auto assign = dynamic_cast<Assign<Ref>*>(instruction)) {
               assm.mov(r10, current_refs());
               assm.mov(r10, M64{r10, (uint32_t)assign->src.num * STACK_VALUE_SIZE});
-              write_temp(assign->dest.num, r10);
+              write_temp(assign->dest, r10);
             } else if (auto assign = dynamic_cast<Assign<Deref>*>(instruction)) {
               assm.mov(r10, current_refs());
               assm.mov(r10, M64{r10, (uint32_t)assign->src.num * STACK_VALUE_SIZE});
-              write_temp(assign->dest.num, M64{r10});
+              write_temp(assign->dest, M64{r10});
             } else if (auto assign = dynamic_cast<Assign<Glob>*>(instruction)) {
               uint64_t addr = VM::Value::makePointer(&closure).value;
               assm.mov(r10, Imm64{addr});
               assm.mov(r11, Imm64{(uint32_t)assign->src.num});
               call_helper((void *)(&helper_read_global), r10, r11);
-              write_temp(assign->dest.num, rax);
+              write_temp(assign->dest, rax);
             } else if (auto assign = dynamic_cast<Assign<IR::Function>*>(instruction)) {
               uint64_t addr = VM::Value::makePointer(&closure).value;
               assm.mov(r10, Imm64{addr});
               assm.mov(r11, Imm64{(uint32_t)assign->src.num});
               call_helper((void *)(&helper_read_function), r10, r11);
-              write_temp(assign->dest.num, rax);
+              write_temp(assign->dest, rax);
             }
             break;
           }
           case IR::Operation::Store: {
             if (auto store = dynamic_cast<Store<Var>*>(instruction)) {
-              read_temp(store->src.num, r10);
-              write_local(store->dest.num, r10);
+              read_temp(store->src, r10);
+              write_local(store->dest, r10);
             } else if (auto store = dynamic_cast<Store<Deref>*>(instruction)) {
               assm.mov(r10, current_refs());
               assm.mov(r10, M64{r10, (uint32_t)store->dest.num * STACK_VALUE_SIZE});
-              read_temp(store->src.num, r11);
+              read_temp(store->src, r11);
               assm.mov(M64{r10}, r11);
             } else if (auto store = dynamic_cast<Store<Glob>*>(instruction)) {
               uint64_t addr = VM::Value::makePointer(&closure).value;
               assm.mov(r10, Imm64{addr});
               assm.mov(r11, Imm64{(uint32_t)store->dest.num});
-              read_temp(store->src.num, rax);
+              read_temp(store->src, rax);
               call_helper((void *)(&helper_write_global), r10, r11, rax);
             }
             break;
           }
           case IR::Operation::Add: {
             auto add = dynamic_cast<Add*>(instruction);
-            read_temp(add->src1.num, r10);
-            read_temp(add->src2.num, r11);
+            read_temp(add->src1, r10);
+            read_temp(add->src2, r11);
             call_helper((void *)(&helper_add), r10, r11);
-            write_temp(add->dest.num, rax);
+            write_temp(add->dest, rax);
             break;
           }
           case IR::Operation::Sub: {
             #warning account for tag bits
             auto sub = dynamic_cast<Sub*>(instruction);
-            read_temp(sub->src1.num, r10);
-            read_temp(sub->src2.num, r11);
+            read_temp(sub->src1, r10);
+            read_temp(sub->src2, r11);
             assm.sub(r10, r11);
-            write_temp(sub->dest.num, r10);
+            write_temp(sub->dest, r10);
           }
           case IR::Operation::Mul: {
             #warning account for tag bits
             auto mul = dynamic_cast<Mul*>(instruction);
-            read_temp(mul->src1.num, r10);
-            read_temp(mul->src2.num, r11);
+            read_temp(mul->src1, r10);
+            read_temp(mul->src2, r11);
             assm.imul(r10, r11);
-            write_temp(mul->dest.num, r10);
+            write_temp(mul->dest, r10);
           }
           case IR::Operation::Neg: {
             #warning account for tag bits
             auto neg = dynamic_cast<Neg*>(instruction);
-            read_temp(neg->src.num, r10);
+            read_temp(neg->src, r10);
             assm.neg(r10);
-            write_temp(neg->dest.num, r10);
+            write_temp(neg->dest, r10);
+          }
+          case IR::Operation::Jump: {
+            auto jump = dynamic_cast<Jump*>(instruction);
+            assm.jmp(Rel32{jump->delta});
+          }
+          case IR::Operation::CondJump: {
+            auto cjump = dynamic_cast<CondJump*>(instruction);
+            uint64_t true_val = VM::Value::makeBoolean(true).value;
+            read_temp(cjump->cond, r10);
+            assm.cmp(r10, Imm32{(uint32_t)true_val});
+            assm.je(Rel32{cjump->delta});
           }
         }
       }
