62f61a7872c94e9665dcbd551e7a117a53c37900
Jack Serrino <jserrino@gmail.com>
2017-05-10 00:54:38 -0400

    don't reverse unnecessarily

--- mitscript/ir/Compiler.h
+++ mitscript/ir/Compiler.h
@@ -163,7 +163,7 @@ namespace IR {
             auto arg2 = popTemp();
             instructions.push_back(new CallHelper<Helper::AssertInt>{arg1});
             instructions.push_back(new CallHelper<Helper::AssertInt>{arg2});
-            instructions.push_back(new CallHelper<Helper::AssertNotZero>{arg2});
+            instructions.push_back(new CallHelper<Helper::AssertNotZero>{arg1});
             instructions.push_back(new Div{nextTemp(), arg2, arg1});
             break;
           }
@@ -226,7 +226,6 @@ namespace IR {
             for (size_t i = 0; i < ref_count; ++i) {
               refs.push_back(popTemp());
             }
-            reverse(refs.begin(), refs.end());
             instructions.push_back(new AllocClosure{function, refs});
             assign(make_shared<RetVal>());
             break;
